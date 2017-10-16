d4cd54e90dd4e8e046e1c6c551d56e7ccaae31d6
ShaToyaJones <jshatoyaj@yahoo.com>
2017-10-15 22:51:50 -0400

    Created the Word and Letter js files. Pseudocoded out the Hangman game.

---
+++ hangman.js
@@ -0,0 +1,53 @@
+*****This is a game of Hangman using the command line and constructor functions.*****
+
+//Create a Word constructor function in its own file.
+	//File has been created.
+
+
+//Create a Letter constructor function in its own file.
+	//File has been created.
+
+
+//Export the Word and Letter files.
+
+
+
+*****Hangman Pseudocode*****
+//User is created/create a user.
+
+
+//The body parts of the hangman are needed:
+	//head
+	//neck
+	//body
+	//left arm
+	//right arm
+	//left leg
+	//right leg
+		//game over
+
+
+//Start the game.
+	//A word is randomly selected.
+		//possibly based on a category.
+			//Let the user know how many letters the word is.
+
+
+
+//User guesses a letter.
+
+
+//The letter that the user guesses is displayed in a container.
+
+
+//The application checks to see if the letter chosen is apart of the word.
+	//if the letter is apart of the word
+			//the letter is displayed in its correct position of the word.
+		//else
+			//one part of the hangmans body is revealed.
+
+//Keep track of the users remaining guesses.
+	//possibly display them to the user after each incorrect guess.
+
+
+	//Prompt the user if they would like to end the game once they do not have any attmpts left.
